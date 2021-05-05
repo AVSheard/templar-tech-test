1785fc8d4de78e083cbb96398e5eb1fe31872ea9
AVSheard <a_sheard@yahoo.co.uk>
2021-05-05 14:37:09 +0100

    got basic test to pass

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 const cors = require("cors");
 
 app.use(cors());
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use("/:id", getUser);
+const getUser = (request, response, next) => {
+	response.send(users);
+};
 
-const getUser = (request, response, next) => {};
+app.use("/:id", getUser);
 
 const users = [
 	{
