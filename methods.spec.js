01b34bf9dab55e1348d034ee260421e17c108ee4
AVSheard <a_sheard@yahoo.co.uk>
2021-05-05 21:57:29 +0100

    updated first test

--- methods.spec.js
+++ methods.spec.js
@@ -3,8 +3,8 @@ const { expect } = require("chai");
 const { filterByUsername, addKey } = require("./methods");
 
 describe("filterByUsername", () => {
-	it("returns [] when passed []", () => {
-		const actual = filterByUsername([]);
+	it("returns [] when passed [] and empty string", () => {
+		const actual = filterByUsername([], "");
 		const expected = [];
 		expect(actual).to.eql(expected);
 	});
