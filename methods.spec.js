012047aed24e71af1e1ac649902c252012b67e12
AVSheard <a_sheard@yahoo.co.uk>
2021-05-05 21:56:00 +0100

    wrote test to return empty array when passed empty array

--- methods.spec.js
+++ methods.spec.js
@@ -1,3 +1,11 @@
 const { expect } = require("chai");
 
 const { filterByUsername, addKey } = require("./methods");
+
+describe("filterByUsername", () => {
+	it("returns [] when passed []", () => {
+		const actual = filterByUsername([]);
+		const expected = [];
+		expect(actual).to.eql(expected);
+	});
+});
