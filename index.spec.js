3139d2255b759e9ecf29797e75eda7c9f418aef4
AVSheard <a_sheard@yahoo.co.uk>
2021-05-05 14:41:20 +0100

    wrote test for specific user

--- index.spec.js
+++ index.spec.js
@@ -7,4 +7,15 @@ describe("/:id", () => {
 	it("GET - 200 for successful request for user", () => {
 		return request(app).get("/1").expect(200);
 	});
+	it("GET - returns requested user object", () => {
+		return request(app)
+			.get("/1")
+			.then((res) => {
+				expect(res.body.user).to.equal({
+					id: 1,
+					username: "admin",
+					password: "test",
+				});
+			});
+	});
 });
