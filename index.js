204b88c771283a005b047547f4f2416d3180ee84
AVSheard <a_sheard@yahoo.co.uk>
2021-05-05 11:05:00 +0100

    rearanged index folder

--- index.js
+++ index.js
@@ -1,25 +1,12 @@
 const express = require("express");
 const app = express();
-const bodyParser = require("body-parser");
 const cors = require("cors");
 
 app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-/** 
-* Brief,
+app.use(express.json());
 
-Create a GET enpoint that returns a 
-user from the array from the urlPARAM id
-* Create an POST endpoint that accepts
-*  a username and password and then 
-*  returns the user object below if
-   the password is the same
+app.use("/:id", getUser);
 
-Create an POST enpoint that adds 
-username and password to the
-users array
-*/
 const users = [
 	{
 		id: 0,
