1045535ff8163c23f1afa7b36d0f657de5c710b4
AVSheard <a_sheard@yahoo.co.uk>
2021-05-06 08:57:06 +0100

    wrote test to handle multiple array entries that need to be removed and kept

--- methods.spec.js
+++ methods.spec.js
@@ -28,7 +28,7 @@ describe("filterByUsername", () => {
 		];
 		expect(actual).to.eql(expected);
 	});
-	it("returns given array with unrequested entries removed", () => {
+	it("returns given array with single unrequested entry removed", () => {
 		const actual = filterByUsername(
 			[
 				{
@@ -49,6 +49,46 @@ describe("filterByUsername", () => {
 				id: 0,
 				username: "test",
 				password: "test",
+			},
+		];
+		expect(actual).to.eql(expected);
+	});
+	it("returns given array with multiple unrequested entries removed", () => {
+		const actual = filterByUsername(
+			[
+				{
+					id: 0,
+					username: "test",
+					password: "test",
+				},
+				{
+					id: 1,
+					username: "admin",
+					password: "test",
+				},
+				{
+					id: 3,
+					username: "test",
+					password: "password",
+				},
+				{
+					id: 4,
+					username: "anthony",
+					password: "12345",
+				},
+			],
+			"test"
+		);
+		const expected = [
+			{
+				id: 0,
+				username: "test",
+				password: "test",
+			},
+			{
+				id: 3,
+				username: "test",
+				password: "password",
 			},
 		];
 		expect(actual).to.eql(expected);
